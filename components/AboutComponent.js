3840675a4ca9f170b3a4c86bb00d673d3ec92454
codewithbingle <codewithbingle@gmail.com>
2022-02-26 10:16:33 -0800

    Components Part 1

---
+++ components/AboutComponent.js
@@ -0,0 +1,10 @@
+import Contact from './ContactComponent'
+import { ScrollView } from " react-native-gesture-handler";
+
+class About extends Component {
+  render() {
+    return <ScrollView></ScrollView>;
+  }
+}
+
+export default About;
---
+++ components/ContactComponent.js
@@ -0,0 +1,14 @@
+import { ScrollView } from "react-native"
+import About from './AboutComponent'
+
+class Contact extends Component {
+  render() {
+    return (
+      <ScrollView>
+
+      </ScrollView>
+    );
+  }
+}
+
+export default Contact;
