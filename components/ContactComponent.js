05b0a52f587ac01eb8997b11649a9398e6255b2b
codewithbingle <codewithbingle@gmail.com>
2022-03-11 21:02:07 -0800

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { setStatusBarStyle } from 'expo-status-bar';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
-
+import * as Animatable from "react-native-animatable";
 class Contact extends Component {
     static navigationOptions = {
         title: 'Contact Us'
@@ -10,27 +10,28 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-                <Card title={'Contact Information'}
-                    wrapperStyle={{ margin: 20 }}
-                >
-                    <Text>
-                        {'1 Nucamp Way'}
-                    </Text>
-                    <Text>
-                        {'Seattle, WA 98001'}
-                    </Text>
-                    <Text style={{ marginBottom: 10 }}>
-                        {'U.S.A'}
-                    </Text>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card title={'Contact Information'}
+                        wrapperStyle={{ margin: 20 }}
+                    >
+                        <Text>
+                            {'1 Nucamp Way'}
+                        </Text>
+                        <Text>
+                            {'Seattle, WA 98001'}
+                        </Text>
+                        <Text style={{ marginBottom: 10 }}>
+                            {'U.S.A'}
+                        </Text>
 
-                    <Text>
-                        {'Phone: 1-206-555-1234'}
-                    </Text>
-                    <Text>
-                        {'Email: campsites@nucamp.co'}
-                    </Text>
-                </Card>
-
+                        <Text>
+                            {'Phone: 1-206-555-1234'}
+                        </Text>
+                        <Text>
+                            {'Email: campsites@nucamp.co'}
+                        </Text>
+                    </Card>
+                 </Animatable.View>
             </ScrollView>
         );
     }
