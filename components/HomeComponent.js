3b4f460bde19ef9996f6affba43e473a7fe2dd46
codewithbingle <codewithbingle@gmail.com>
2022-03-11 20:52:37 -0800

    ANimations

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, Animated } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
@@ -37,43 +37,64 @@ function RenderItem(props) {
   return <View />;
 }
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scaleValue: new Animated.Value(0),
+    };
+  }
 
-    static navigationOptions = {
-        title: 'Home'
-    }
-    render() {
-        return (
-          <ScrollView>
-            <RenderItem
-              item={
-                this.props.campsites.campsites.filter(
-                  (campsite) => campsite.featured
-                )[0]
-              }
-              isLoading={this.props.campsites.isLoading}
-              errMess={this.props.campsites.errMess}
-            />
-            <RenderItem
-              item={
-                this.props.promotions.promotions.filter(
-                  (promotion) => promotion.featured
-                )[0]
-              }
-              isLoading={this.props.promotions.isLoading}
-              errMess={this.props.promotions.errMess}
-            />
-            <RenderItem
-              item={
-                this.props.partners.partners.filter(
-                  (partner) => partner.featured
-                )[0]
-              }
-              isLoading={this.props.partners.isLoading}
-              errMess={this.props.partners.errMess}
-            />
-          </ScrollView>
-        );
-    }
+  animate() {
+    Animated.timing(this.state.scaleValue, {
+      toValue: 1,
+      duration: 1500,
+      useNativeDriver: true,
+    }).start();
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  static navigationOptions = {
+    title: "Home",
+  };
+
+  render() {
+    return (
+      <Animated.ScrollView
+        style={{ transform: [{ scale: this.state.scaleValue }] }}
+      >
+        <RenderItem
+          item={
+            this.props.campsites.campsites.filter(
+              (campsite) => campsite.featured
+            )[0]
+          }
+          isLoading={this.props.campsites.isLoading}
+          errMess={this.props.campsites.errMess}
+        />
+        <RenderItem
+          item={
+            this.props.promotions.promotions.filter(
+              (promotion) => promotion.featured
+            )[0]
+          }
+          isLoading={this.props.promotions.isLoading}
+          errMess={this.props.promotions.errMess}
+        />
+        <RenderItem
+          item={
+            this.props.partners.partners.filter(
+              (partner) => partner.featured
+            )[0]
+          }
+          isLoading={this.props.partners.isLoading}
+          errMess={this.props.partners.errMess}
+        />
+      </Animated.ScrollView>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(Home);
